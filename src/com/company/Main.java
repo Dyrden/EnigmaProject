85b22de9f1814211d0ef207f19b5d09a516932f8
Mark <mark771e@stud.kea.dk>
2022-03-02 09:46:10 +0100

    Added - encryptMessage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
 
     public static void main(String[] args) {
         new Main().run();
@@ -8,11 +11,47 @@ public class Main {
 
 
     public void run() {
+        System.out.println(encryptMessage("TESTER", 5));
+
+        /*
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+
+        System.out.println("Input text to encrypt");
+        input = sc.nextLine();
+
+        if (input.equalsIgnoreCase("encrypt")) {
+            System.out.println("Input text to encrypt");
+            input = sc.nextLine();
+
+
+            // run encrypting
+
+        } else {
+
+            // run decrypting
+        }
+
+
+        System.out.println("Input text to encrypt");
+        int shift = sc.nextInt();
+
+        * */
 
     }
 
 
 
+    public String encryptMessage(String str, int shift) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char character = str.charAt(i);
+            char shiftedCharacter = findLetterByNumber(findNumberByLetter(character) + shift);
+            sb.append(shiftedCharacter);
+        }
+        return sb.toString();
+    }
 
     final String ALPHABET = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
 
